5a832e82f93ab93f0398464a7aa2525da273336d
Richard Keller <richard@briefmedia.com>
2018-03-23 23:45:19 -0500

    fixed zip script and removed deprecated tags from style.css

--- create_zip.sh
+++ create_zip.sh
@@ -2,6 +2,7 @@
 rm -rf /tmp/molecular;
 mkdir /tmp/molecular;
 cp -r . /tmp/molecular;
-rm -rf /tmp/molecular/.git
-cd /tmp/molecular
+rm -rf /tmp/molecular/.git /tmp/molecular/tmp /tmp/molecular/create_zip.sh;
+cd /tmp/molecular;
+rm ~/Desktop/molecular.zip
 zip -r ~/Desktop/molecular.zip *;
