d3f67dd7d067b8ba1f0581a3a5fb709009dfb41a
Richard Keller <richard@briefmedia.com>
2018-03-23 23:09:34 -0500

    ready for review

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,7 @@
 jQuery(document).ready(function($){
-
-
 	$('#masthead input[type=search]').focus(function(){
 		$('#masthead .search-form').addClass('focus');
 	}).blur(function(){
 		$('#masthead .search-form').removeClass('focus');
 	});
-
-	console.log( Object.keys( window ) );
-	console.log( Object.keys( document ) );
-
 });
---
+++ create_zip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir temp;
+cp -r . temp
