f4e2f932f5de34288114fbb9e4144cdba297e0f5
Richard Keller <richard@briefmedia.com>
2018-03-25 12:12:22 -0500

    added testing cp

--- create_zip.sh
+++ create_zip.sh
@@ -6,3 +6,5 @@ rm -rf /tmp/molecular/.git /tmp/molecular/tmp /tmp/molecular/create_zip.sh;
 cd /tmp/molecular;
 rm ~/Desktop/molecular.zip
 zip -r ~/Desktop/molecular.zip *;
+rm -rf /Users/richard/www/personal/wordpress/wp/wp-content/themes/molecular;
+cp -r /tmp/molecular /Users/richard/www/personal/wordpress/wp/wp-content/themes;
